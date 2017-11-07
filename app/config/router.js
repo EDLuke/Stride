6ffba42512227992a92649820e81a996c07e6b8b
Luke Zhang <lz2504@columbia.edu>
2017-11-07 15:30:05 -0500

    Added icons for the tab navigator

--- app/config/router.js
+++ app/config/router.js
@@ -8,6 +8,8 @@ import Social from '../screens/Social';
 import Setting from '../screens/Setting';
 import Signup from '../screens/Signup';
 import SocialDetail from '../screens/SocialDetail';
+import { MaterialCommunityIcons, 
+SimpleLineIcons, FontAwesome } from '@expo/vector-icons';
 
 export const StackNavSocial = StackNavigator({
 	Social:{
@@ -16,8 +18,8 @@ export const StackNavSocial = StackNavigator({
 	SocialDetail:{
 		screen: SocialDetail,
 		navigationOptions: ({navigation}) => ({
-      	title: `${navigation.state.params.name}'s Profile`
-    }),
+      		title: `${navigation.state.params.name}'s Profile`
+   		}),
 
 	}
 },
@@ -28,26 +30,51 @@ export const StackNavSocial = StackNavigator({
 export const TabNav = TabNavigator({
 	Today:{
 		screen: Today,
-		tabBarIcon: () => (
-			<MaterialCommunityIcons name="home" size={16} color="#FFF"/>
-		)
+		navigationOptions : ({navigation}) => ({
+			tabBarIcon: ({ tintColor }) => (
+				<MaterialCommunityIcons name="home" size={25} color={tintColor}/>
+			),
+		}),
+		
 	},
 	Charts:{
 		screen: Charts,
+		navigationOptions : ({navigation}) => ({
+			tabBarIcon: ({ tintColor }) => (
+				<SimpleLineIcons name="graph" size={25} color={tintColor}/>
+			),
+		}),
 	},
 	Social:{
 		screen: StackNavSocial,
+		navigationOptions : ({navigation}) => ({
+			tabBarIcon: ({ tintColor }) => (
+				<FontAwesome name="users" size={23} color={tintColor}/>
+			),
+		}),	
 	},
 	Settings:{
 		screen: Setting,
+		navigationOptions : ({navigation}) => ({
+			tabBarIcon: ({ tintColor }) => (
+				<MaterialCommunityIcons name="account-circle" size={25} color={tintColor}/>
+			),
+		}),
 	},
 },
 {
 	headerMode: 'float',
 	tabBarPosition: 'bottom',
 	tabBarOptions:{
+		showIcon: true,
+		showLabel: false,
 		activeTintColor: "#FFF",
 		inactiveTintColor: "#FFF",
+		iconStyle:{
+			margin: -12,
+			height: 30,
+			width: 55,
+		},
 		labelStyle: {
 			margin: 0,
 			fontSize: 12,

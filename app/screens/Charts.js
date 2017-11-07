572fdc6fae6ad1ba1e3833c7be2ab026a73a8afb
Yang He <yh2825@columbia.edu>
2017-11-07 14:49:13 -0500

    tmp

--- app/screens/Charts.js
+++ app/screens/Charts.js
@@ -1,49 +1,275 @@
 import React from 'react';
 import {Font} from 'expo';
 import { Button, FlatList, ScrollView, StyleSheet, Text, View } from 'react-native';
-import User from '../components/class/UserClass.js'
+import User from '../components/class/UserClass.js';
+import { SmoothLine } from 'react-native-pathjs-charts';
 
 GLOBAL = require('../components/CurrentUser');
 
 export default class Charts extends React.Component{
 	state = {
+    records: [
+      {
+        'date':'10-17',
+        'calorie':'1500',
+        goal:'1500'
+      },
+      {
+        'date':'10-18',
+        'calorie':'2000',
+        goal:'1500'
+      },
+      {
+        'date':'10-19',
+        'calorie':'3000',
+        goal:'1700'
+      },
+      {
+        'date':'10-20',
+        'calorie':'1000',
+        goal:'1500'
+      },
+      {
+        'date':'10-21',
+        'calorie':'1500',
+        goal:'1300'
+      },
+      {
+        'date':'10-22',
+        'calorie':'2500',
+        goal:'1500'
+      },
+      {
+        'date':'10-23',
+        'calorie':'1500',
+        goal:'1700'
+      }
+    ],
+    options: {
+      width: 280,
+      height: 280,
+      color: '#2980B9',
+      margin: {
+        top: 20,
+        left: 45,
+        bottom: 25,
+        right: 20
+      },
+      animate: {
+        type: 'delayed',
+        duration: 200
+      },
+      axisX: {
+        showAxis: true,
+        showLines: true,
+        showLabels: true,
+        showTicks: true,
+        zeroAxis: false,
+        orient: 'bottom',
+        label: {
+          fontFamily: 'Arial',
+          fontSize: 14,
+          fontWeight: true,
+          fill: '#34495E'
+        }
+      },
+      axisY: {
+        showAxis: true,
+        showLines: true,
+        showLabels: true,
+        showTicks: true,
+        zeroAxis: false,
+        orient: 'left',
+        label: {
+          fontFamily: 'Arial',
+          fontSize: 14,
+          fontWeight: true,
+          fill: '#34495E'
+        }
+      }
+    }
 	};
-  
-  // async componentDidMount(){
-  //   /* Load the assets for Expo app*/
-  //   await Font.loadAsync({
-  //     'Helvetica': require('../../assets/fonts/Helvetica.ttf'),
-  //   });
-
-  //   this.setState({fontLoaded: true});
-  // }
 
 	render() {
+    let data = [
+      [{
+        "x": -10,
+        "y": -1000
+      }, {
+        "x": -9,
+        "y": -729
+      }, {
+        "x": -8,
+        "y": -512
+      }, {
+        "x": -7,
+        "y": -343
+      }, {
+        "x": -6,
+        "y": -216
+      }, {
+        "x": -5,
+        "y": -125
+      }, {
+        "x": -4,
+        "y": -64
+      }, {
+        "x": -3,
+        "y": -27
+      }, {
+        "x": -2,
+        "y": -8
+      }, {
+        "x": -1,
+        "y": -1
+      }, {
+        "x": 0,
+        "y": 0
+      }, {
+        "x": 1,
+        "y": 1
+      }, {
+        "x": 2,
+        "y": 8
+      }, {
+        "x": 3,
+        "y": 27
+      }, {
+        "x": 4,
+        "y": 64
+      }, {
+        "x": 5,
+        "y": 125
+      }, {
+        "x": 6,
+        "y": 216
+      }, {
+        "x": 7,
+        "y": 343
+      }, {
+        "x": 8,
+        "y": 512
+      }, {
+        "x": 9,
+        "y": 729
+      }, {
+        "x": 10,
+        "y": 1000
+      }],
+      [{
+        "x": -10,
+        "y": 100
+      }, {
+        "x": -9,
+        "y": 81
+      }, {
+        "x": -8,
+        "y": 64
+      }, {
+        "x": -7,
+        "y": 49
+      }, {
+        "x": -6,
+        "y": 36
+      }, {
+        "x": -5,
+        "y": 25
+      }, {
+        "x": -4,
+        "y": 16
+      }, {
+        "x": -3,
+        "y": 9
+      }, {
+        "x": -2,
+        "y": 4
+      }, {
+        "x": -1,
+        "y": 1
+      }, {
+        "x": 0,
+        "y": 0
+      }, {
+        "x": 1,
+        "y": 1
+      }, {
+        "x": 2,
+        "y": 4
+      }, {
+        "x": 3,
+        "y": 9
+      }, {
+        "x": 4,
+        "y": 16
+      }, {
+        "x": 5,
+        "y": 25
+      }, {
+        "x": 6,
+        "y": 36
+      }, {
+        "x": 7,
+        "y": 49
+      }, {
+        "x": 8,
+        "y": 64
+      }, {
+        "x": 9,
+        "y": 81
+      }, {
+        "x": 10,
+        "y": 100
+      }]
+    ]
+
+    let options = {
+      width: 280,
+      height: 280,
+      color: '#2980B9',
+      margin: {
+        top: 20,
+        left: 45,
+        bottom: 25,
+        right: 20
+      },
+      animate: {
+        type: 'delayed',
+        duration: 200
+      },
+      axisX: {
+        showAxis: true,
+        showLines: true,
+        showLabels: true,
+        showTicks: true,
+        zeroAxis: false,
+        orient: 'bottom',
+        label: {
+          fontFamily: 'Arial',
+          fontSize: 14,
+          fontWeight: true,
+          fill: '#34495E'
+        }
+      },
+      axisY: {
+        showAxis: true,
+        showLines: true,
+        showLabels: true,
+        showTicks: true,
+        zeroAxis: false,
+        orient: 'left',
+        label: {
+          fontFamily: 'Arial',
+          fontSize: 14,
+          fontWeight: true,
+          fill: '#34495E'
+        }
+      }
+    }
+    
 		return (
-			<View style = {styles.container}>
-            <View style={styles.textBoxContainer}>
-				    {
-            	<Text style={styles.title}>Visualize...</Text>
-        		}
-        		{
-        			<Text style={styles.subtitle}>During the last 7 days</Text>
-        		}
-            </View>
-        		{
-        		<View style={styles.textBoxContainer}>
-        				<Text style={styles.boxtitle}>Calorie Output</Text>
-        		</View>
-            }
-            {
-        			<View style={styles.chartBoxContainer}>
-        				<ScrollView>
-        					<Text style = {styles.boxtext}>
-                    {GLOBAL.currentUser.username}'s Chart
-                  </Text>
-        				</ScrollView>
-        			</View>
-        		}
-			</View>
+			<View style={styles.container}>
+        <SmoothLine data={data} options={options} xKey='x' yKey='y' />
+      </View>
 		);
 	}
 }
